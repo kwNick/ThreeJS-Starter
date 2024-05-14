e4310266c66dcfb79ebc1b6318edac8d826c6519
Nickolas Piraino <kwnp17c@gmail.com>
2024-05-14 02:06:46 -0400

    drawing lines with vectors

--- main.js
+++ main.js
@@ -5,8 +5,10 @@ import WebGL from 'three/addons/capabilities/WebGL.js';
 if (WebGL.isWebGLAvailable()) {
 
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth
-        / window.innerHeight, 0.1, 1000);
+    const camera = new THREE.PerspectiveCamera(45, window.innerWidth
+        / window.innerHeight, 1, 500);
+    camera.position.set(0, 0, 100);
+    camera.lookAt(0, 0, 0);
 
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
@@ -16,13 +18,22 @@ if (WebGL.isWebGLAvailable()) {
     const material = new THREE.MeshBasicMaterial({ color: 0xff6ff8 });
 
     const cube = new THREE.Mesh(geo, material);
-    scene.add(cube);
-    camera.position.z = 5;
+    // scene.add(cube);
+    // camera.position.z = 5;
 
+    const materialLine = new THREE.LineBasicMaterial({ color: 0x0000ff });
+    const points = [];
+    points.push(new THREE.Vector3(- 10, 0, 0));
+    points.push(new THREE.Vector3(0, 10, 0));
+    points.push(new THREE.Vector3(10, 0, 0));
+
+    const geoLine = new THREE.BufferGeometry().setFromPoints(points);
+    const line = new THREE.Line(geoLine, materialLine);
+    scene.add(line);
     function animate() {
         requestAnimationFrame(animate);
-        cube.rotation.x += 0.05;
-        cube.rotation.y += 0.03;
+        // cube.rotation.x += 0.05;
+        // cube.rotation.y += 0.03;
         renderer.render(scene, camera);
     }
     animate();
