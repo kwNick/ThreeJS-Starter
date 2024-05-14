1e5e66b8c35d2145e7e9ffc2c8462c34ef4b8474
Nickolas Piraino <kwnp17c@gmail.com>
2024-05-14 01:52:48 -0400

    creating a fallback if the browser doesnt have webgl compatibility; 0xff6ff8

--- main.js
+++ main.js
@@ -1,24 +1,34 @@
 import * as THREE from 'three';
+import WebGL from 'three/addons/capabilities/WebGL.js';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth
-    / window.innerHeight, 0.1, 1000);
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+if (WebGL.isWebGLAvailable()) {
 
-const geo = new THREE.BoxGeometry(1, 1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth
+        / window.innerHeight, 0.1, 1000);
 
-const cube = new THREE.Mesh(geo, material);
-scene.add(cube);
-camera.position.z = 5;
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
 
-function animate() {
-    requestAnimationFrame(animate);
-    cube.rotation.x += 0.05;
-    cube.rotation.y += 0.13;
-    renderer.render(scene, camera);
+    const geo = new THREE.BoxGeometry(1, 1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({ color: 0xff6ff8 });
+
+    const cube = new THREE.Mesh(geo, material);
+    scene.add(cube);
+    camera.position.z = 5;
+
+    function animate() {
+        requestAnimationFrame(animate);
+        cube.rotation.x += 0.05;
+        cube.rotation.y += 0.03;
+        renderer.render(scene, camera);
+    }
+    animate();
+
+} else {
+
+    const warning = WebGL.getWebGLErrorMessage();
+    document.getElementById('container').appendChild(warning);
 }
-animate();
